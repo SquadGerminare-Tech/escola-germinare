48674751825ac3f375071525d7b82e91e36124cf
gfsfef <gabrielfelixpinho@gmail.com>
2022-11-18 21:24:42 -0300

    criando o sistema de carrossel

---
+++ src/components/Carrossel/index.tsx
@@ -0,0 +1,36 @@
+import './style.css'
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useRef, useState, useEffect } from 'react'
+
+interface tipo {
+    lista: Array<{}>
+}
+
+const ComponentCarrossel = (props: tipo) => {
+    const carrossel: any = useRef()
+    const [tamanho, setTamanho] = useState(0)
+
+    useEffect(() => {
+        setTamanho(carrossel.current?.scrollWidth - carrossel.current?.offsetWidth)
+    }, [])
+
+    return(
+        <div className='container'>
+            <motion.div ref={carrossel}className='carrossel' whileTap={{ cursor: 'grabbing' }}>
+                <motion.div 
+                className='inner'
+                drag={'x'}
+                dragConstraints={{ right: 0, left: -tamanho}}>
+                    {props.lista.map((item: any) => (
+                        <motion.div className='item'>
+                            {item}
+                        </motion.div>
+                    ))}
+                </motion.div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default ComponentCarrossel
--- src/components/SobreNos/index.tsx
+++ src/components/SobreNos/index.tsx
@@ -1,13 +1,24 @@
 import Hero from './Hero/index'
 import Instituto from './Instituto/index'
 import ProjetoAcedmico from './ProjetoAcademico'
+import ComponentCarrossel from '../Carrossel/index'
 
 const SobreNos = () => {
+  let lista: Array<any> = [
+  <div>Tetse1</div>, 
+  <div>Tetse2</div>, 
+  <div>Tetse3</div>, 
+  <div>Tetse4</div>, 
+  <div>Tetse5</div>,
+  <div>Tetse6</div>, 
+  <div>Tetse7</div>
+]
   return (
     <>
       <Hero titulo={'Transformando o Brasil através da educação'} />
       <Instituto />
       <ProjetoAcedmico />
+      <ComponentCarrossel lista={lista}/>
     </>
   )
 }

4b835c83de6d7db5d160f0063a0b5b0ddbc90bc3
anna <annaalufc231106@gmail.com>
2022-12-01 20:21:58 -0300

    socorrooooo

--- src/components/SobreNos/SobreGerminare/style.tsx
+++ src/components/SobreNos/SobreGerminare/style.tsx
@@ -1,20 +1,23 @@
 import { styled } from '@mui/system'
 
 const AboutGerminare = styled('section')({
-    backgroundColor: '#E8E7E7',
-    padding: '60px 100px',
-    '& .text': {
-      display: 'flex',
-      gap: '2rem',
-      flexDirection: 'column'
-    },
-    '& .h2': {
-      color: '#2E3092',
-      fontSize: '2em'
-    },
-    '& .p': {
-      fontSize: '1.125em'
-    }
-  })
+  backgroundColor: '#E8E7E7',
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '5rem',
+  padding: '60px 100px',
+  '& .text': {
+    display: 'flex',
+    gap: '2rem',
+    flexDirection: 'column'
+  },
+  '& .h2': {
+    color: '#2E3092',
+    fontSize: '2em'
+  },
+  '& .p': {
+    fontSize: '1.125em'
+  }
+})
 
 export default AboutGerminare

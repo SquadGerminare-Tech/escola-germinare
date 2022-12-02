43cb370a293b588e056d285711fee9a5a744daec
David Viana <david.viana@germinare.org.br>
2022-12-02 08:39:55 -0300

    fixing css

--- src/components/Section_Celular/index.tsx
+++ src/components/Section_Celular/index.tsx
@@ -1,34 +1,26 @@
 import * as React from 'react';
-import {BackgroundSection, ContainerTxt, ContainerCelular} from './style'
+import { BackgroundSection, ContainerTxt, ContainerCelular } from './style'
 import $ from 'jquery'
 
 interface Section {
     title: string,
     children: React.ReactNode,
-    
+
 }
 
 const Section_Celular = (props: Section) => {
-    return(
+    return (
         <>
             <BackgroundSection>
-
                 <div>
-
-                <ContainerTxt>
-                    <p className='titleSection' >{props.title}</p>
-                    <p>{props.children}</p>
-                </ContainerTxt>
-
-                <ContainerCelular>
-
-                </ContainerCelular>
-                    
+                    <ContainerTxt>
+                        <p className='titleSection' >{props.title}</p>
+                        <p>{props.children}</p>
+                    </ContainerTxt>
+                    <ContainerCelular>
+                    </ContainerCelular>
                 </div>
-                
-
             </BackgroundSection>
-
         </>
     )
 }
